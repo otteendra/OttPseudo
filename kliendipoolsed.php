05dd184296504b389b2d38bb9a35db71a5df9d48
otteendra <ott-erik.eendra@khk.ee>
2015-09-23 12:22:03 +0300

    5. Kass koeraks

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -10,7 +10,22 @@
 
 <a href="http://www.khk.ee" onclick="alert('Tere maailm')">Tere maailm!</a>
 
-<a href="" onclick="alert('Jääme siia!')">Jääme siia!</a>
+<a href="" onclick="alert('Jääme siia!')">Jääme siia!</a> <br>
+
+<img id="myImage" onclick="changeImage()" src="http://i.dailymail.co.uk/i/pix/2014/10/15/1413408700148_wps_39_image001_jpg.jpg" width="140" height="100">
+
+<script>
+    function changeImage() {
+        var image = document.getElementById('myImage');
+        if (image.src.match("bulbon")) {
+            image.src = "http://i.dailymail.co.uk/i/pix/2014/10/15/1413408700148_wps_39_image001_jpg.jpg";
+        } else {
+            image.src = "https://www.petfinder.com/wp-content/uploads/2012/11/122163343-conditioning-dog-loud-noises-632x475.jpg";
+        }
+    }
+</script>
+
+
 
 </body>
 </html>
